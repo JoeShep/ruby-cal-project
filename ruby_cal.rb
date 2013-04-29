6fc5cd614bff376df6dd360762f3940a2d74ebeb
Joe Shepherd <joeshepmedia@gmail.com>
2013-04-29 11:42:36 -0500

    Add integration tests for edge cases

--- ruby_cal.rb
+++ ruby_cal.rb
@@ -4,3 +4,18 @@ year = ARGV[1]
 
 puts `cal #{month} #{year}`
 
+#pseudo code and notes
+
+# def month_year_header(month, year)
+#     puts  "     #{month} #{year}"
+#   end
+
+# adapted from wiki pseudocode
+# if year is divisible by 400 then
+#    is_leap_year
+# elsif year is divisible by 100 then
+#    not_leap_year
+# elsif year is divisible by 4 then
+#    is_leap_year
+# else
+#    not_leap_year
